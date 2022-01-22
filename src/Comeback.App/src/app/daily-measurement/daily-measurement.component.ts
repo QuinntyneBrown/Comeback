427e49d61f74312aa6c8a79f4a07f1684229ef42
QuinntyneBrown <quinntynebrown@gmail.com>
2022-01-22 03:58:22 -0500

    update

--- src/Comeback.App/src/app/daily-measurement/daily-measurement.component.ts
+++ src/Comeback.App/src/app/daily-measurement/daily-measurement.component.ts
@@ -3,22 +3,34 @@ import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { DailyMeasurement } from '@api';
 import { DailyMeasurementService } from '@api/services';
-import { Destroyable, NavigationService } from '@core';
-import { of } from 'rxjs';
-import { map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { NavigationService } from '@core';
+import { combineLatest, of, Subject } from 'rxjs';
+import { map, startWith, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-daily-measurement',
   templateUrl: './daily-measurement.component.html',
   styleUrls: ['./daily-measurement.component.scss']
 })
-export class CreateDailyMeasurementComponent extends Destroyable  {
+export class CreateDailyMeasurementComponent  {
   
+  private readonly _cancelSubject: Subject<void> = new Subject();
+
+  private readonly _cancel$ = this._cancelSubject.asObservable();
+
+  private readonly _saveSubject: Subject<DailyMeasurement> = new Subject();
+
+  private readonly _save$ = this._saveSubject.asObservable();
+
   readonly formControl = new FormControl();
 
-  readonly vm$ = this._activatedRoute.paramMap
+  readonly vm$ = combineLatest([
+    this._activatedRoute.paramMap,
+    this._cancel$.pipe(tap(_ => this._navigationService.redirectToDefault()), startWith(null)),
+    this._save$.pipe(switchMap(d => this._handleSave(d)), startWith(null))    
+  ]) 
   .pipe(
-    map(paramMap => paramMap.get("id")),
+    map(([paramMap]) => paramMap.get("id")),
     switchMap(dailyMeasurementId => dailyMeasurementId ? this._dailyMeasurementService.getById({ dailyMeasurementId }) : of(null)),
     map(dailyMeasurement => {
       return {
@@ -31,27 +43,26 @@ export class CreateDailyMeasurementComponent extends Destroyable  {
     private readonly _dailyMeasurementService: DailyMeasurementService,
     private readonly _navigationService: NavigationService,
     private readonly _activatedRoute: ActivatedRoute
-  ) { 
-    super();
-  }
+  ) { }
 
-  save(dailyMeasurement: DailyMeasurement): void {
-
+  private _handleSave(dailyMeasurement: DailyMeasurement) {
     const obs$ = dailyMeasurement.dailyMeasurementId
     ? this._dailyMeasurementService.update({ dailyMeasurement})
     : this._dailyMeasurementService.create({
       dailyMeasurement
     });
 
-    obs$
+    return obs$
     .pipe(
-      takeUntil(this._destroyed$),
       tap(_ => this._navigationService.redirectToDefault())
     )
-    .subscribe();
+  }
+
+  save(dailyMeasurement: DailyMeasurement) {
+    this._saveSubject.next(dailyMeasurement);
   }
 
   cancel() {
-    this._navigationService.redirectToDefault();
+    this._cancelSubject.next();
   }
 }
